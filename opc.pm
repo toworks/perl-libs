04197b1c36ad4a26800767150ac5bc600dd1eb76
Admin <Admin@KRR-FAS46.europe.mittalco.com>
2019-03-26 11:19:20 +0200

    add property to opc group

--- opc.pm
+++ opc.pm
@@ -32,11 +32,14 @@ package opc;{
 	my($self) = @_;
     eval{ 	$self->{opc}->{opcintf} = undef;
 			$self->{opc}->{opcintf} = Win32::OLE::OPC->new('OPC.Automation',
-												  $self->{opc}->{name}#,
+												  $self->{opc}->{name},
 												  $self->{opc}->{host}
 												  )	or die "$!";
-			$self->{opc}->{opcintf}->MoveToRoot	or die "$!";
+#			$self->{opc}->{opcintf}->MoveToRoot	or die "$!";
 			$self->{opc}->{group} = $self->{opc}->{opcintf}->OPCGroups->Add($self->{opc}->{group_name})	or die "$!";
+			$self->{opc}->{group}->SetProperty('UpdateRate', 500);
+			$self->{opc}->{group}->SetProperty('IsActive', 1);
+			$self->{opc}->{group}->SetProperty('IsSubscribed', 1);
 			$self->{opc}->{items} = $self->{opc}->{group}->OPCItems or die "$!";
 	};
 	if($@) { $self->{opc}->{error} = 1;
